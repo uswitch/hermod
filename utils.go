882841b2bb51efd60f90309563dd910cf6c82765
jaysiyani <jaysiyani>
2021-06-24 11:49:17 +0100

    update makefile to include all go files and uncomment wip docker build

---
+++ utils.go
@@ -0,0 +1,17 @@
+package main
+
+import log "github.com/sirupsen/logrus"
+
+func configureLogger(logLevel string) {
+	switch logLevel {
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	case "info":
+		log.SetLevel(log.InfoLevel)
+	case "warn":
+		log.SetLevel(log.WarnLevel)
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	}
+	log.SetFormatter(&log.JSONFormatter{})
+}

4c39668d96e610e9edce8d583efd3fe07759f1cd
jaysiyani <jaysiyani>
2021-06-24 11:06:35 +0100

    add basic deployment watcher

--- main.go
+++ main.go
@@ -48,3 +48,17 @@ func main() {
 
 	watcher.Run(ctx, stopCh)
 }
+
+func configureLogger(logLevel string) {
+	switch logLevel {
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	case "info":
+		log.SetLevel(log.InfoLevel)
+	case "warn":
+		log.SetLevel(log.WarnLevel)
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	}
+	log.SetFormatter(&log.JSONFormatter{})
+}
